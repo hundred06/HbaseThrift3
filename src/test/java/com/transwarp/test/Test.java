8b89f1ac28ed4a50f574f8f76b7e394f68f42ef8
hundred06 <kingpeng21@163.com>
2021-01-08 10:46:53 +0800

    增加测试东西

--- src/test/java/com/transwarp/test/Test.java
+++ src/test/java/com/transwarp/test/Test.java
@@ -1,12 +1,19 @@
 package com.transwarp.test;
 
+import org.slf4j.LoggerFactory;
+
 import java.util.logging.Logger;
 
 public class Test {
-    private static final Logger logger = Logger.getLogger(Test.class);
+    private static final Logger logger = (Logger) LoggerFactory.getLogger(Test.class);
 
+    @
     public void test1(){
         System.out.println("jfoeawijofiewojfowejioajifeoij");
+        logger.info("fjeoajofeijoaijofejaoifjoej");
+
+        System.out.println();
+
     }
 
 }
